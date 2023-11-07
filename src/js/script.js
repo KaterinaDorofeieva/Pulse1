5a1d47bdb84b5b600a4e7cd3faa8ed36fff9d2fa
Kate <ketkatarina14@gmail.com>
2023-11-07 16:30:51 +0200

    new section

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+/* if (2 * 6 == 8 * 1) {
+    console.log("True")
+} else {
+    console.log("False")
+}
+
+let answer = confirm("Вам есть 18?")
+if (answer) {
+    console.log('Welcome')
+} else {
+    console.log ('Go')
+} */
+
+for (let i = 1; i < 8; i++){
+    console.log(i);
+}
